9a4d95659c02fb40ac7e427b3f16ed2583787ee6
lorenzo.tomasini <lorenzo.tomasini@fokus.fraunhofer.de>
2015-09-22 19:17:13 +0200

    removed mongodb scripts

--- install-config.sh
+++
@@ -1,8 +0,0 @@
-echo "--------------INSTALL---------------"
-
-sudo apt-get update && sudo apt-get install screen
-export LC_ALL=C
-screen -S shard mongod --configsvr --dbpath /data/db --port 27019 --smallfiles &
-
-echo "--------------DONE---------------"
-exit 0
--- install-shard.sh
+++
@@ -1,8 +0,0 @@
-echo "--------------INSTALL---------------"
-
-sudo apt-get update && sudo apt-get install screen
-export LC_ALL=C
-screen -S shard mongo --shardsrv --smallfiles --port 27017 &
-
-echo "--------------DONE---------------"
-exit 0
