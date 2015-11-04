a610a0edca3709810b00c3eda8b2b94801350d91
root <michael.pauls@fokus.fraunhofer.de>
2015-11-04 13:41:29 +0000

    changed to unix file

--- server_configure_floatingIp.sh
+++ server_configure_floatingIp.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ $server_shared_int_network_floatingIp ]
-then
-    screen -d -m -S client iperf -c $server_shared_int_network_floatingIp -t 300
+if [ -n $server_shared_int_network_floatingIp ]; then
+ screen -d -m -S client iperf -c $server_shared_int_network_floatingIp -t 300
 else
-    screen -d -m -S client iperf -c $server_private_floatingIp -t 300
+ screen -d -m -S client iperf -c $server_private_floatingIp -t 300
 fi
+
