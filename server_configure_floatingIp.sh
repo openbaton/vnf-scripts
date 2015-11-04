838f2edb6ecfba6502e598f63d3c06500e6d8e2c
Lorenzo Tomasini <lorenzo.tomasini@fokus.fraunhofer.de>
2015-11-04 13:18:02 +0100

    updated script networks

--- server_configure_floatingIp.sh
+++ server_configure_floatingIp.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-screen -d -m -S client iperf -c $server_shared_int_network_floatingIp -t 300
+if [ $server_shared_int_network_floatingIp ]
+then
+    screen -d -m -S client iperf -c $server_shared_int_network_floatingIp -t 300
+else
+    screen -d -m -S client iperf -c $server_private_floatingIp -t 300
+fi
