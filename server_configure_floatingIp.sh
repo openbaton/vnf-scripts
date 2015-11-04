cb548f66bd85f7159851e0fbff74617b1a0eacd1
root <lorenzo.tomasini@fokus.fraunhofer.de>
2015-11-04 14:27:20 +0000

    fixed condition

--- server_configure_floatingIp.sh
+++ server_configure_floatingIp.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-if [ -n $server_shared_int_network_floatingIp ]; then
- screen -d -m -S client iperf -c $server_shared_int_network_floatingIp -t 300
+if [ $server_shared_int_network_floatingIp ]
+then
+    screen -d -m -S client iperf -c $server_shared_int_network_floatingIp -t 300
 else
- screen -d -m -S client iperf -c $server_private_floatingIp -t 300
+    screen -d -m -S client iperf -c $server_private_floatingIp -t 300
 fi
 
