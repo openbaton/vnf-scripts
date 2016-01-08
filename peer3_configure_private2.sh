0fcafb4c9fa558c362fb46fe841c7e7246fba7be
Thomas Briedigkeit <thomas.briedigkeit@fokus.fraunhofer.de>
2016-01-08 11:06:33 +0100

    corrected scripts for complex integration test scenario

--- peer3_configure_private2.sh
+++ peer3_configure_private2.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 echo 'peer3_private2='$peer3_private2 > /home/ubuntu/params
-echo 'peer5_private2='$peer5_private2 >> /home/ubuntu/params
 
 screen -d -m -S client3 iperf -c $peer3_private2 -t 300
-screen -d -m -S client5 iperf -c $peer5_private2 -t 300
