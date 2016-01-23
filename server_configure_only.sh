616e71338f1e024d0515fba24621900ed3a3b83f
Marcello Monachesi <marcello.monachesi@fokus.fraunhofer.de>
2016-01-23 17:53:08 +0100

    Fixed strange characters

--- server_configure_only.sh
+++ server_configure_only.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-#Check if there are screens up and deleted them
+#Check if there are screens up and deletd them
 result=$(screen -ls | grep client | wc -l);
 if [ "${result}" -ne "0" ]; then
-        echo "client running, it will be killed"
-        pkill screen
-        exit;
+		echo "client running, it will be killed"
+		pkill screen
 fi
+
 #Clean existing configuration files
 if [ -f /root/iperf-server-ip ]; then
-    echo "deleting config file"
-    rm /root/iperf-server-ip
+	echo "deleting config file"
+	rm /root/iperf-server-ip
 fi
 
 #Configuration
 touch /root/iperf-server-ip
-echo "$server_private" >  /root/iperf-server-ip
+echo "$server_private" > /root/iperf-server-ip
+
