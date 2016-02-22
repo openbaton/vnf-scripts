06ceca8847c2ee381feb43375f3c93729305cdb4
michael.pauls <michael.pauls@mailbox.tu-berlin.de>
2016-02-22 02:04:32 +0100

    Improved elasticApp

--- elasticapp-install-hello-world.sh
+++ elasticapp-install-hello-world.sh
@@ -3,6 +3,8 @@
 FILE_NAME="file.php"
 TEST_FILE="/var/www/html/$FILE_NAME"
 
+SCRIPT_FILE="/root/helloworld.sh"
+
 if [ -z ${STRESS_SECONDS+x} ]; then
   echo "STRESS_SECONDS is unset";
   STRESS_SECONDS="60"
@@ -10,18 +12,21 @@ else
   echo "STRESS_SECONDS is set to '$STRESS_SECONDS'";
 fi
 
-if [ -z ${CORES+x} ]; then
-  echo "CORES is unset";
-  CORES="4"
+if [ -z ${LOOP+x} ]; then
+  echo "LOOP is unset";
+  LOOP="1000"
 else
-  echo "CORES is set to '$CORES'";
+  echo "LOOP is set to '$LOOP'";
 fi
 
 sudo apt-get update
-sudo apt-get install apache2 stressapptest -y
+sudo apt-get install apache2 -y
 sudo apt-get install php5 -y
 sudo apt-get install libapache2-mod-php5 -y
 sudo /etc/init.d/apache2 restart
+
+echo '#!/bin/bash' >> $SCRIPT_FILE
+echo 'for i in {1..'"$LOOP"'}; do md5sum <(HelloWorld) ; done' >> $SCRIPT_FILE
 
 
 echo "<?php" >> $TEST_FILE
@@ -29,8 +34,7 @@ echo "header('Content-Type: text/plain');" >> $TEST_FILE
 echo 'echo "Server IP: {$_SERVER['SERVER_ADDR']}";' >> $TEST_FILE
 echo 'echo "\nClient IP: {$_SERVER['REMOTE_ADDR']}";' >> $TEST_FILE
 echo 'echo "\nX-Forwarded-for: {$_SERVER['HTTP_X_FORWARDED_FOR']}\n";' >> $TEST_FILE
-echo "shell_exec('for i in {1..$CORES}; do echo Hello World ; done');" >> $TEST_FILE
+echo "shell_exec('bash $SCRIPT_FILE');" >> $TEST_FILE
 echo "?>" >> $TEST_FILE
 
 cat $TEST_FILE
-
