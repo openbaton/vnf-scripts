147985e331ad4f655e76b7bd005c67637222ac7e
Lorenzo Tomasini <lorenzo.tomasini@gmail.com>
2016-06-10 12:13:43 +0200

    Create zabbix_configure_agent.sh

---
+++ zabbix_configure_agent.sh
@@ -0,0 +1,20 @@
+
+#!/bin/bash
+
+if [[ -z "$private_floatingIp" ]]; then
+  echo "private_floatingIp trying with private"
+  if [[ -z "$private_floatingIp" ]]; then
+    echo "private is required"
+    exit 1
+  else
+    export MONITORING_IP=$private
+  fi
+else
+  export MONITORING_IP=$private_floatingIp  
+fi
+
+echo "Installing zabbix-agent for server at $MONITORING_IP"
+sudo apt-get install -y zabbix-agent
+sudo sed -i -e "s/ServerActive=127.0.0.1/ServerActive=$MONITORING_IP:10051/g" -e "s/Server=127.0.0.1/Server=$MONITORING_IP/g" -e "s/Hostname=Zabbix server/#Hostname=/g" /etc/zabbix/zabbix_agentd.conf
+sudo service zabbix-agent restart
+echo "finished installing zabbix-agent!"
