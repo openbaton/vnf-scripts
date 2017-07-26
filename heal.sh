1c4167dbf3d9dbf2dd3d3247055c4a7cdeeb8c05
Marcello Monachesi <marco.mantis@gmail.com>
2017-07-26 13:20:26 +0200

    change login: now it heals a SipP server
    This script is linked from this NSD http://openbaton.github.io/documentation/descriptors/tutorial-sipp-fms/sipp-fms-nsd.json.
    Which is part of the FMS tutorial in this official page: http://openbaton.github.io/documentation/fms-sipp-tutorial/

--- heal.sh
+++ heal.sh
@@ -2,14 +2,14 @@
 
 case "$cause" in
 
-("Iperf-server-down") 
-	echo "The Iper-server is down, let's try to restart it..."
-	screen -d -m -S server iperf -s
+("serviceDown")
+	echo "SipP server is down, let's try to restart it..."
+	screen -d -m -S server sipp -sn uas
 	if [ $? -ne 0 ]; then
-	    echo "ERROR: the iperf-server is not started"
+	    echo "ERROR: SipP server does not start"
 	    exit 1
     	fi
-	echo "The Iper-server is running again!"
+	echo "SipP server is running again!"
 	;;
 *) echo "The cause $cause is unknown"
 	exit 2
