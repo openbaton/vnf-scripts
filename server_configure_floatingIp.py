f50d99e63c5362993ea6c217f8fc40895f7feb9c
Oleksander Gozman <oleksander.gozman@fokus.fraunhofer.de>
2017-09-13 14:27:03 +0200

    fix: rename

--- server_configure_floatingIp.py
+++ server_configure_floatingIp.py
@@ -2,7 +2,7 @@
 import subprocess
 
 def main():
-    proc = subprocess.Popen("screen -d -m -S server iperf -s".split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    proc = subprocess.Popen("screen -d -m -S client iperf -c $server_private_floatingIp -t 300".split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out1, err1 = proc.communicate()
     return out1
 
